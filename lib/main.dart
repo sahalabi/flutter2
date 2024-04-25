e9ae32f0e8147553a05413310b94c99263aba332
sahalabi <sahalapp59@gmail.com>
2024-04-25 10:41:10 +0530

    project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter2/thrdscrn.dart';
 
 import 'frstscrn.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home:Screen1(
+      home: screen3(
+      ),
 
-      )
 
-    ),
+      ),
+
+
   );
 }
 
---
+++ lib/thrdscrn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class screen3 extends StatelessWidget {
+  const screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+
+    );
+  }
+}
+
+
